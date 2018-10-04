66020a9f8504bd5916a90ae0f9ae29f51c7ea1ab
dondish <odedshapira1@gmail.com>
2018-10-04 11:51:19 +0300

    accept 2 letter class names

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,7 +1,7 @@
 const database = require('./database')
 const XRegExp = require('xregexp')
 
-const nameregex = XRegExp.build('^.{4,45}$')
+const nameregex = XRegExp.build('^.{2,45}$')
 
 exports.addClass = async (name) => {
     if (!nameregex.test(name)) {
